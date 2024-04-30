14f26dcd5f0702836233ab261dfc00ac32b57a4f
ItssMasonn <159991874+ItssMasonn@users.noreply.github.com>
2024-04-29 23:14:57 -0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,7 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
-        // See if the interaction is from someone with the id 407317071857188884
+        // See if the interaction is from someone with the id 407317071857188884 
         if (message.author.id === '407317071857188884') {
             message.reply(
                 '**This man is corrupt. Do not trust what he says.**'
