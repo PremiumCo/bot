add99398cee76ef0eb640e2ca10d38eb2b1f38de
Dylan <49600150+hrtzg@users.noreply.github.com>
2024-04-30 21:48:05 -0700

    Spare velcro

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,8 +4,7 @@ module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
         if (
-            message.author.id === '407317071857188884' ||
-            message.author.id === '925565687592783882'
+            message.author.id === '407317071857188884'
         ) {
             await message.reply(
                 "**rawr :3 you must bow down to the supreme leader like a good boy or you'll stay banished UwU**\nhttps://tenor.com/view/cute-sorry-anime-tears-gif-14764586"
